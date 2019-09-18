3d526d7326efb5aa08b36f5a72f21e78d35acef3
Iain Tooley <iaintooley1@gmail.com>
2019-09-18 10:50:39 +0100

    tested played has name and lives

---
+++ Player.rb
@@ -0,0 +1,10 @@
+class Player
+
+  attr_reader(:name, :lives)
+
+  def initialize(name, lives)
+    @name = name
+    @lives = lives
+  end
+
+end
--- specs/Player_specs.rb
+++ specs/Player_specs.rb
@@ -4,10 +4,16 @@ require_relative("../Player")
 
 class TestPlayer < MiniTest::Test
 
-  def setup
+  def setup()
+    @player = Player.new("Bob", 6)
+  end
 
-    
+  def test_player_has_name
+    assert_equal("Bob", @player.name)
+  end
 
+  def test_player_has_lives
+    assert_equal(6, @player.lives)
   end
 
 end
